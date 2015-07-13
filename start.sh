86db6171e6a821c9bcca7fd518373303707686ef
tropicloud <admin@tropicloud.net>
2015-07-12 23:12:00 -0300

    first commit

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ if [[  ! -z $MYSQL_ENV_MYSQL_ROOT_PASSWORD  ]]; then
 
 	echo -e "\033[0;30m
 -----------------------------------------------------
-\033[0;34m  Adminer\033[0;37m - Database management\033[0;30m
+\033[0;34m  Adminer\033[0;37m - Database Management\033[0;30m
 -----------------------------------------------------
 \033[0m
-\033[0m  MYSQL HOST:\033[0;37m $MYSQL_PORT_3306_TCP_ADDR
-\033[0m  MYSQL USER:\033[0;37m root
-\033[0m  MYSQL PASS:\033[0;37m $MYSQL_ENV_MYSQL_ROOT_PASSWORD
+\033[0m  MySQL Host:\033[0;37m $MYSQL_PORT_3306_TCP_ADDR
+\033[0m  MySQL User:\033[0;37m root
+\033[0m  MySQL Pass:\033[0;37m $MYSQL_ENV_MYSQL_ROOT_PASSWORD
 \033[0m"
 
 fi
