19c65c03d9a7ef3ec13ceaca9e6a478879bb1b0d
tropicloud <admin@tropicloud.net>
2015-11-11 13:43:20 -0200

    DB search/replace

--- start.sh
+++ start.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 if [[  ! -z $MYSQL_ENV_MYSQL_ROOT_PASSWORD  ]]; then
-
 	echo -e "\033[0;30m
------------------------------------------------------
 \033[0;34m  Adminer\033[0;37m - Database Management\033[0;30m
------------------------------------------------------
-\033[0m
+-----------------------------------------------------\n
 \033[0m  MySQL Host:\033[0;37m $MYSQL_PORT_3306_TCP_ADDR
-\033[0m  MySQL User:\033[0;37m root
-\033[0m  MySQL Pass:\033[0;37m $MYSQL_ENV_MYSQL_ROOT_PASSWORD
-\033[0m"
-
+\033[0m  MySQL Pass:\033[0;37m $MYSQL_ENV_MYSQL_ROOT_PASSWORD\n"
 fi
 
-exec php -S 0.0.0.0:80 -t /adminer
+exec php -S 0.0.0.0:80 -t /app
